a291f3a0e7f0ff3da85507f822128b96711c6684
Codding BBQ <codingbbq@gmail.com>
2020-10-22 22:17:34 +0530

    Custom popup

--- lib/pages/home/utils/popup_dialog.dart
+++ lib/pages/home/utils/popup_dialog.dart
@@ -3,11 +3,6 @@ import 'package:flutter/material.dart';
 class PopUpDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.70,
-        width: MediaQuery.of(context).size.width * 0.90,
-      ),
-    );
+    return Container();
   }
 }
